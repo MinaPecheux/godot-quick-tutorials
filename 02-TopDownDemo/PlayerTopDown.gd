07d3c6b91484994df0dbaff4e50728580c81c5b6
Mina Pêcheux <mina.pecheux@gmail.com>
2024-06-11 14:08:32 +0200

    feat(core): add code+assets for tutorial 02+03 - 2D top down movement+2D camera follow

---
+++ 02-TopDownDemo/PlayerTopDown.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+
+func _physics_process(_delta):
+	var direction = Input.get_vector(
+		'ui_left', 'ui_right',
+		'ui_up', 'ui_down')
+	if direction.length():
+		velocity = direction * SPEED
+		look_at(position + direction)
+	else:
+		velocity.x = move_toward(
+			velocity.x, 0, SPEED)
+		velocity.y = move_toward(
+			velocity.y, 0, SPEED)
+	move_and_slide()
