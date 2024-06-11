3eea7357313fbb421f0de43273275842020998ee
Mina Pêcheux <mina.pecheux@gmail.com>
2024-06-11 14:19:19 +0200

    fix(misc): add namespaces + improve formatting of some scripts

--- 02-TopDownDemo/PlayerTopDown.cs
+++ 02-TopDownDemo/PlayerTopDown.cs
@@ -1,20 +1,27 @@
 using Godot;
-public partial class PlayerTopDown : CharacterBody2D {
-	public const float Speed = 400.0f;
-	public override void _PhysicsProcess(double delta) {
-		Vector2 velocity = Velocity;
-		Vector2 direction = Input.GetVector(
-			"ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero) {
-			velocity = direction * Speed;
-			LookAt(Position + direction);
-		} else {
-			velocity.X = Mathf.MoveToward(
-				Velocity.X, 0, Speed);
-			velocity.Y = Mathf.MoveToward(
-				Velocity.Y, 0, Speed);
+
+namespace Tutorial02_TopDownMovement {
+
+	public partial class PlayerTopDown : CharacterBody2D {
+
+		public const float Speed = 400.0f;
+
+		public override void _PhysicsProcess(double delta) {
+			Vector2 velocity = Velocity;
+			Vector2 direction = Input.GetVector(
+				"ui_left", "ui_right", "ui_up", "ui_down");
+			
+			if (direction != Vector2.Zero) {
+				velocity = direction * Speed;
+				LookAt(Position + direction);
+			} else {
+				velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+				velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+			}
+
+			Velocity = velocity;
+			MoveAndSlide();
 		}
-		Velocity = velocity;
-		MoveAndSlide();
 	}
+
 }
--- 02-TopDownDemo/PlayerTopDown.gd
+++ 02-TopDownDemo/PlayerTopDown.gd
@@ -4,14 +4,13 @@ const SPEED = 400.0
 
 func _physics_process(_delta):
 	var direction = Input.get_vector(
-		'ui_left', 'ui_right',
-		'ui_up', 'ui_down')
+		'ui_left', 'ui_right', 'ui_up', 'ui_down')
+
 	if direction.length():
 		velocity = direction * SPEED
 		look_at(position + direction)
 	else:
-		velocity.x = move_toward(
-			velocity.x, 0, SPEED)
-		velocity.y = move_toward(
-			velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
 	move_and_slide()
