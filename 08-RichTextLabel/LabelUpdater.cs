09c329e1ae0b939be00884171ed8da9b5f09c3cc
Mina Pêcheux <mina.pecheux@gmail.com>
2024-06-20 14:44:50 +0200

    fix(misc): add namespace to CS script

--- 08-RichTextLabel/LabelUpdater.cs
+++ 08-RichTextLabel/LabelUpdater.cs
@@ -1,21 +1,25 @@
 using Godot;
 
-public partial class LabelUpdater : RichTextLabel {
+namespace Tutorial08_RichTextLabel {
 
-	private bool _stylised = false;
+	public partial class LabelUpdater : RichTextLabel {
 
-	public override void _Input(InputEvent @event) {
-		if (@event is InputEventKey k && !k.Pressed &&
-			k.Keycode == Key.Space) {
-			_stylised = !_stylised;
-			if (_stylised)
-				Text = "[center]Let's [i][font_size=100]do[/font_size][/i]"
-					+ " some [b][color=#aa0000]styling[/color][/b] in "
-					+ "[img=80]res://icon.svg[/img]![/center]";
-			else
-				Text = "[center]Let's do some styling "
-					+ "in [img=80]res://icon.svg[/img]![/center]";
+		private bool _stylised = false;
+
+		public override void _Input(InputEvent @event) {
+			if (@event is InputEventKey k && !k.Pressed &&
+				k.Keycode == Key.Space) {
+				_stylised = !_stylised;
+				if (_stylised)
+					Text = "[center]Let's [i][font_size=100]do[/font_size][/i]"
+						+ " some [b][color=#aa0000]styling[/color][/b] in "
+						+ "[img=80]res://icon.svg[/img]![/center]";
+				else
+					Text = "[center]Let's do some styling "
+						+ "in [img=80]res://icon.svg[/img]![/center]";
+			}
 		}
+
 	}
 
 }
